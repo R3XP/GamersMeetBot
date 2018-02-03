27e79c7d36c17f8ae1f8ecad01bb3f50229ede30
R3XP <30810131+R3XP@users.noreply.github.com>
2018-02-03 23:41:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,10 @@ const Discord = require("discord.js");
 const Bot = new Discord.Client();
 
 Bot.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${Bot.user.tag}!`);
 });
-
-Bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
 
 
 
 //MUST STAY:
-Bot.login(process.env.BOT_TOKEN),
+Bot.login(process.env.BOT_TOKEN);
